d6ab9becaf1542610bbac5583d7bd0e82ddecd68
Jaquan Piper <jaquan.piper@gmail.com>
2020-03-24 00:03:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
-# code here!
+require 'pry'
+
+class School 
+  
+  def initialize(school_name)
+    @school_name = school_name 
+    @roster = {} 
+  end 
+  
+  def roster 
+    @roster 
+  end 
+  
